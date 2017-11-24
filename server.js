6a32c2e7e70d433bef2b2cf82067eeeee20a4877
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2017-11-24 09:14:50 +0100

    Create draft for posts lists

--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const path = require('path');
+const mime = require('mime-types');
 
-http.createServer(function (request, response) {
-    console.log(request.url);
+http.createServer((request, response) => {
+    let url = request.url.replace(/(\?.*)|(\/\.\.)/, ""),
+        cwd = process.cwd(),
+        body = "OK",
+        path = cwd + (url === '/' ? '/index.html' : url);
 
-
-
-    if (request.url === '/') {
-        let file = fs.readFile('./posts.html', function (error,data) {
-            response.writeHead(200, { "Content-Type": "text/html; charset=utf-8;" });
-            response.write(data);
-            response.end();
-        });
-    }
-    else{
-        response.writeHead(200, { "Content-Type": "text/plain; charset=utf-8;" });
-        response.write("OK");
+    fs.lstat(path, (error, status) => {
+        if(!error && status){
+            if (status.isFile()) {
+                const fileContentType = mime.lookup(path);
+                response.writeHead(200, { 'Content-Type': fileContentType });
+                body = fs.readFileSync(path);
+                response.write(body);
+            } else {
+                response.writeHead(200,{'Content-Type' : 'text/plain; charset=utf-8'})
+                response.write(body);
+            }
+        } else{
+            response.writeHead(404, { 'Content-Type': "text/plain" });
+            response.write(`${request.url} not found`);
+        }      
         response.end();
-    }    
+    });
+
+
+
 }).listen(8080)
