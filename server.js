06c07db014f704c788b7f3839d5421479a27f1f4
Zbigniew Jędraczka <mr.zbigniew@gmail.com>
2017-11-27 10:14:02 +0100

    Fixes in posts list to handle put and delete operations

--- server.js
+++ server.js
@@ -4,30 +4,31 @@ const path = require('path');
 const mime = require('mime-types');
 
 http.createServer((request, response) => {
-    console.log(request.url);
     let url = request.url.replace(/(\?.*)|(\/\.\.)/, ""),
         cwd = process.cwd(),
         body = "OK",
         path = cwd + (url === '/' ? '/index.html' : url);
 
     fs.lstat(path, (error, status) => {
-        if(!error && status){
+        if (!error && status) {
             if (status.isFile()) {
                 const fileContentType = mime.lookup(path);
                 response.writeHead(200, { 'Content-Type': fileContentType });
                 body = fs.readFileSync(path);
                 response.write(body);
             } else {
-                response.writeHead(200,{'Content-Type' : 'text/plain; charset=utf-8'})
+                response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
                 response.write(body);
             }
-        } else{
+        } else {
             response.writeHead(404, { 'Content-Type': "text/plain" });
             response.write(`${request.url} not found`);
-        }      
+        }
         response.end();
     });
 
 
 
-}).listen(8080)
+}).listen(8080, function () {
+    console.log("Server is listening at port 8080");
+});
